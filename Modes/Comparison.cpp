2c6053812bbf7651110dd75f1ce9c717c92ed3d2
phongvt2004 <ntp11892vt@gmail.com>
2023-07-20 17:28:57 +0700

    comparison mode cmd4 and gendata type

--- Modes/Comparison.cpp
+++ Modes/Comparison.cpp
@@ -4,15 +4,44 @@
 #include "Mode.cpp"
 
 class Comparison : public Mode{
+    private:
+        ull run_time_0;
+        ull number_comparison_0;
+        char** argv = NULL;
+        char* input_file = NULL;
+        int input_size = -1;
+        int input_order = -1;
     protected:
 
-        void ShowResult(char output[]) {
-
+        void ShowResult(char output[] = NULL) {
+            cout << "COMPARISON MODE: " << endl;
+            cout << "Algorithm: " << this->argv[2] << " | " << this->argv[3]<< endl;
+            if(this->input_file) cout << "Input file: " << this->input_file << endl;
+            cout << "Input size: " << this->input_size << endl;
+            if(this->input_order != -1) cout << "Input order: " << this->input_order << endl;
+            cout << "-----------------------" << endl;
+            cout << "Running time: " << this->run_time_0 << " | " << this->run_time << endl;
+            cout << "Comparison: " << this->number_comparison_0 << " | " << this->number_comparison << endl;
         }
 
     public:
         void Run(int argc, char* argv[]) {
-
+            this->argv = argv;
+            if(argv[4][0] >= '0' and argv[4][0] <= '9') {
+                cout << "Waiting for phat..." << endl;
+                return;
+            }
+            this->input_file=argv[4];
+            this->sortName = this->getSortName(argv[2]);
+            this->getSortAlgorithm(this->input_file);
+            sort->Run(this->run_time, this->number_comparison);
+            this->run_time_0 = this->run_time;
+            this->number_comparison_0 = this->number_comparison;
+            delete this->sort;
+            this->sortName = this->getSortName(argv[3]);
+            this->getSortAlgorithm(this->input_file);
+            sort->Run(this->run_time, this->number_comparison);
+            ShowResult();
         }
 
         using Mode::Mode;
--- Modes/Mode.cpp
+++ Modes/Mode.cpp
@@ -2,15 +2,60 @@
 #define MODE
 
 #include "../Definitions/Datatype.cpp"
+#include "../Definitions/SortAlgorithm.cpp"
 #include "../Algorithms/Sort.h"
 
 class Mode {
 private:
+    
+protected:
     ull run_time;
     ull number_comparison;
     Sort *sort;
+    char *sortName;
+    char* getSortName(char input[]) {
+        char * sortName = new char[10];
+        int i=0;
+        while(input[i] != '-') {
+            sortName[i] = input[i];
+            i++;
+        }
+        sortName[i] = '\0';
+        return sortName;
+    };
 
-protected:
+    void getSortAlgorithm(const char* fileinput) {
+        if(strcmp(this->sortName, "selection")==0) sort = new SelectionSort(fileinput);
+        
+        else if(strcmp(this->sortName, "insertion")==0) sort = new InsertionSort(fileinput);
+        
+        else if(strcmp(this->sortName, "bubble")==0) sort = new BubbleSort(fileinput);
+        
+        
+        else if(strcmp(this->sortName, "shaker")==0) sort = new ShakerSort(fileinput);
+        
+        
+        else if(strcmp(this->sortName, "shell")==0) sort = new ShellSort(fileinput);
+        
+        
+        else if(strcmp(this->sortName, "heap")==0) sort = new HeapSort(fileinput);
+        
+        
+        else if(strcmp(this->sortName, "merge")==0) sort = new MergeSort(fileinput);
+        
+        
+        else if(strcmp(this->sortName, "quick")==0) sort = new QuickSort(fileinput);
+        
+        
+        else if(strcmp(this->sortName, "counting")==0) sort = new CountingSort(fileinput);
+        
+        else if(strcmp(this->sortName, "radix")==0) sort = new RadixSort(fileinput);
+        
+        else if(strcmp(this->sortName, "flash")==0) sort = new FlashSort(fileinput);
+        
+        else return;
+    }
+    
     virtual void ShowResult(char output[]) = 0;
 
 public:
@@ -24,6 +69,7 @@ public:
 
     ~Mode() {
         delete sort;
+        delete[] sortName;
     }
 };
 
